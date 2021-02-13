a45257c48c91497300bd6ae36648f7ffff05ae26
David Charatan <david.charatan@gmail.com>
2021-02-12 21:36:56 -0500

    Clean up main.cpp a bit.

--- main.cpp
+++ main.cpp
@@ -12,35 +12,33 @@ using namespace std::chrono;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
-    ////////////////////////////////////////////////////////////////////////////////
     QCommandLineParser parser;
     parser.addHelpOption();
     parser.addPositionalArgument("infile", "Input .obj file path");
     parser.addPositionalArgument("outfile", "Output .obj file path");
     parser.addPositionalArgument("method", "subdivide/simplify/remesh/denoise");
 
-    //Subdivide: number of iterations
-    //Simplify: number of faces to remove
-    //Remesh: number of iterations
-    //Denoise: number of iterations
+    // Subdivide: number of iterations
+    // Simplify: number of faces to remove
+    // Remesh: number of iterations
+    // Denoise: number of iterations
     parser.addPositionalArgument("args1", "respective argument for the method");
 
-    //Remesh: Tangential smoothing weight
-    //Denoise: Smoothing parameter 1 (\Sigma_c)
+    // Remesh: Tangential smoothing weight
+    // Denoise: Smoothing parameter 1 (\Sigma_c)
     parser.addPositionalArgument("args2", "respective argument2 for the method");
 
-    //Denoise: Smoothing parameter 2 (\Sigma_s)
+    // Denoise: Smoothing parameter 2 (\Sigma_s)
     parser.addPositionalArgument("args3", "respective argument3 for the method");
 
-    //Denoise: Kernel size (\rho)
+    // Denoise: Kernel size (\rho)
     parser.addPositionalArgument("args4", "respective argument4 for the method");
 
     parser.process(a);
 
     const QStringList args = parser.positionalArguments();
     if(args.size() < 3) {
-        cerr << "Error: Wrong number of arguments" << endl;
+        cerr << "Arguments <input .obj file path> <output .obj file path> <method (subdivide, simplify, remesh or denoise)> <method-specific arguments...>" << endl;
         a.exit(1);
         return 1;
     }
@@ -48,37 +46,30 @@ int main(int argc, char *argv[])
     QString outfile = args[1];
     QString method = args[2];
 
-    ////////////////////////////////////////////////////////////////////////////////
-
     Mesh m;
     m.loadFromFile(infile.toStdString());
 
     auto t0 = high_resolution_clock::now();
-    // TODO
-    // Convert the mesh into your own data structure
 
-    // TODO
-    // Implement the operations
+    // TODO: Convert the mesh into your own data structure.
+    // TODO: Implement the operations.
     if (method == "subdivide"){
-        //TODO
+        // TODO
     } else if (method == "simplify"){
-        //TODO
+        // TODO
     } else if (method == "remesh"){
-        //TODO
+        // TODO
     } else if (method == "denoise") {
-        //TODO
+        // TODO
     } else {
-        cerr << "Error: Unknown method name" << endl;
+        cerr << "Error: Unknown method \"" << method.toUtf8().constData() << "\"" << endl;
     }
 
     auto t1 = high_resolution_clock::now();
-    auto duration = duration_cast<milliseconds>(t1-t0).count();
+    auto duration = duration_cast<milliseconds>(t1 - t0).count();
+    cout << "Execution took " << duration << " milliseconds." <<endl;
 
-    cout << "Execution takes: " << duration << " milliseconds." <<endl;
-    // TODO
-    // Convert your datastructure back to the basic format
-
-    ////////////////////////////////////////////////////////////////////////////////
+    // TODO: Convert your data structure back to the basic format.
     m.saveToFile(outfile.toStdString());
 
     a.exit();
